0816a68cf4f3ac9f0832ae30a82e9e1d3bc06f5d
sam <samscott640@gmail.com>
2025-05-02 10:20:28 +0800

    Created a basic discord bot, added open/close door messages.

---
+++ cssc-bot/bot.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+
+import dotenv
+import os
+
+dotenv.load_dotenv()
+
+token = os.getenv('TOKEN')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(
+        activity=discord.Activity(type=discord.ActivityType.watching, name="the CSSC Door"))
+    await bot.load_extension('cogs.door')
+
+
+bot.run(token)
---
+++ cssc-bot/cogs/door.py
@@ -0,0 +1,60 @@
+from discord.ext import tasks, commands
+import discord
+
+import requests
+
+import dotenv
+import os
+
+dotenv.load_dotenv()
+
+password = os.getenv('API_PASSWORD')
+
+
+class Door(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        self.ping_door_status.start()
+        self._session = requests.Session()
+        self.current_door_status = None
+
+        self.door_status_channel = self.bot.get_guild(293785967083651074).get_channel(961596047153573948)
+
+
+    def cog_unload(self):
+        self.ping_door_status.cancel()
+
+
+    @tasks.loop(seconds=5.0)
+    async def ping_door_status(self):
+        json_data = self._session.get("https://portal.cssc.asn.au/api/door_status").json()
+        door_status = json_data["door_open"]
+
+        # Only send a message if the status has changed.
+        if door_status != self.current_door_status:
+            self.current_door_status = door_status
+            await self.update_status()
+
+
+
+    async def update_status(self):
+        if self.current_door_status:
+            await self.door_status_channel.send("Door Open")
+        else:
+            await self.door_status_channel.send("Door Closed")
+
+
+    @ping_door_status.before_loop
+    async def before_ping_door_status(self):
+        print("Logging in...")
+        self._session.post('https://portal.cssc.asn.au/api/auth', data={'password': password})
+        print("Successfully logged in: Welcome my leige")
+
+        json_data = self._session.get("https://portal.cssc.asn.au/api/door_status").json()
+        self.current_door_status = json_data["door_open"]
+
+        await self.update_status()
+
+
+async def setup(bot):
+    await bot.add_cog(Door(bot))
